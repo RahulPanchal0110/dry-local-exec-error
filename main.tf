cea4f210721cf6c8a1a4886df556e8df693b6dc8
RahulPanchal0110 <105359148+RahulPanchal0110@users.noreply.github.com>
2025-11-11 13:05:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,2 @@
-terraform {
-  cloud {
-
-    organization = "Rahul-hashicorp-test"
-
-    workspaces {
-      name = "dry-run-local-exec123"
-    }
-  }
-}
-resource "null_resource" "example7" {
-
-
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command = <<EOH
-   aws --version
-EOH
-
-  }
-}
+resource "null_resource" "example7" {}
 
